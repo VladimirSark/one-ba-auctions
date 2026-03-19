814a6e0983efb34772bfdf1a2ba465bde76bdff9
VladimirSark <vsarkov@yahoo.com>
2026-03-19 12:16:16 +0200

    The header panel now shows the real product title:

--- templates/oba-shortcode-custom.php
+++ templates/oba-shortcode-custom.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="oba-shortcode-custom" data-auction-id="<?php echo esc_attr( $product->get_id() ); ?>">
 	<div class="oba-sc-header oba-sc-card">
 		<div class="oba-sc-label">HEADER</div>
-		<div class="oba-sc-placeholder title-placeholder"></div>
+		<h1 class="oba-sc-title"><?php echo esc_html( $product->get_title() ); ?></h1>
 	</div>
 	<div class="oba-sc-left">
 		<div class="oba-sc-card oba-sc-gallery">
