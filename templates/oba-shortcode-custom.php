e19a16c256a902973c6318e30898618cd94c1ab8
VladimirSark <vsarkov@yahoo.com>
2026-03-19 11:57:43 +0200

    Added a Copart-style scaffold with labeled panels so it’s easy to reference:

--- templates/oba-shortcode-custom.php
+++ templates/oba-shortcode-custom.php
@@ -12,19 +12,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="oba-shortcode-custom" data-auction-id="<?php echo esc_attr( $product->get_id() ); ?>">
+	<div class="oba-sc-header oba-sc-card">
+		<div class="oba-sc-label">HEADER</div>
+		<div class="oba-sc-placeholder title-placeholder"></div>
+	</div>
 	<div class="oba-sc-left">
 		<div class="oba-sc-card oba-sc-gallery">
+			<div class="oba-sc-label">MEDIA</div>
 			<div class="oba-sc-placeholder"></div>
 		</div>
 		<div class="oba-sc-card oba-sc-info">
+			<div class="oba-sc-label">DETAILS</div>
 			<div class="oba-sc-placeholder"></div>
 		</div>
 	</div>
 	<div class="oba-sc-right">
 		<div class="oba-sc-card oba-sc-buy">
+			<div class="oba-sc-label">BUY</div>
 			<div class="oba-sc-placeholder"></div>
 		</div>
 		<div class="oba-sc-card oba-sc-auction">
+			<div class="oba-sc-label">AUCTION</div>
 			<div class="oba-sc-placeholder"></div>
 		</div>
 	</div>
